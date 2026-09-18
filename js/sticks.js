fbf3ae13d8a1901cd87559a289674546ae83d0f0
adunukotaashok-2007 <adunukotaashok@gmail.com>
2026-09-18 21:53:08 +0530

    Refactor stick rolling logic and cleanup comments

--- js/sticks.js
+++ js/sticks.js
@@ -4,7 +4,6 @@ const Sticks = {
     roll(callback) {
         const values = [1, 2, 3, 4, 5, 6, 12];
         const weights = [22, 20, 18, 15, 12, 8, 5];
-        
         let totalWeight = weights.reduce((a, b) => a + b, 0);
         let random = Math.random() * totalWeight;
         let result = 1;
@@ -39,16 +38,14 @@ const Sticks = {
                 const pips = s.querySelector('.stick-pips');
                 pips.innerHTML = '';
                 
-                // PIP DOT MAPPING FOR 2 STICKS:
-                // 1 = 1,0 | 2 = 1,1 | 3 = 2,1 | 4 = 2,2 | 5 = 3,2 | 6 = 3,3 | 12 = 0,0 (Bārā!)
                 let dotsCount = 0;
                 if (value === 1) dotsCount = s.dataset.stick === "1" ? 1 : 0;
                 else if (value === 2) dotsCount = 1;
                 else if (value === 3) dotsCount = s.dataset.stick === "1" ? 2 : 1;
                 else if (value === 4) dotsCount = 2;
                 else if (value === 5) dotsCount = s.dataset.stick === "1" ? 3 : 2;
-                else if (value === 6) dotsCount = 3; // Āru = 3 + 3
-                else if (value === 12) dotsCount = 0; // Bārā = 0 + 0
+                else if (value === 6) dotsCount = 3; 
+                else if (value === 12) dotsCount = 0; 
 
                 for (let i = 0; i < dotsCount; i++) {
                     const dot = document.createElement('div');
@@ -63,7 +60,6 @@ const Sticks = {
             if ([1, 6, 12].includes(value)) {
                 display.classList.add('extra-glow');
             }
-
             callback(value);
         }, 600);
     }
