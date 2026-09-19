e4d4e798e0644127f8556d1afd6f24f70e85d3f5
adunukotaashok-2007 <adunukotaashok@gmail.com>
2026-09-19 20:28:44 +0530

    Clean up code by removing unnecessary comment
    Removed comment about animating only the active player's sticks.

--- js/sticks.js
+++ js/sticks.js
@@ -22,8 +22,6 @@ const Sticks = {
 
     animateSticks(value, callback) {
         const currentPlayer = window.Game.gameState.currentPlayer;
-        
-        // ONLY ANIMATE THE ACTIVE PLAYER'S STICKS!
         const sticks = document.querySelectorAll(`#sticksWrapper${currentPlayer} .stick`);
         
         const resultVal = document.getElementById('resultValue');
